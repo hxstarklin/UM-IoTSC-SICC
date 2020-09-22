850bb8b5bdef77a3d045f3e04dd18f7caaee2c56
hxstarklin <mb85410@umac.mo>
2020-09-22 18:49:58 +0800

    update example

--- dgx2/torch/scripts.sh
+++ dgx2/torch/scripts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /your_home_directory/anaconda3/bin/activate your_venv
-cd /your_home_directory/dgx2/torch
-python3 torch_helloworld.py
+source /home/username/anaconda3/bin/activate torch
+cd /home/username/torch
+python3 test.py
--- dgx2/torch/sjob.sh
+++ dgx2/torch/sjob.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name              GroupN_jobname
-#SBATCH --time                  48:00:00
-#SBATCH --gpus                  4
-#SBATCH --output                /your_home_directory/ouput.txt
-#SBATCH --error                 /your_home_directory/error.txt
-
-# asks SLURM to send the USR1 signal 120 seconds before end of the time limit
-#SBATCH --signal=B:USR1@120
-
-# define the handler function
-# note that this is not executed here, but rather
-# when the associated signal is sent
-your_cleanup_function()
-{
-    echo "function your_cleanup_function called at $(date)"
-    # do whatever cleanup you want here
-    pkill -u your_username
-}
-# call your_cleanup_function once we receive USR1 signal
-trap 'your_cleanup_function' USR1
+#SBATCH --job-name              testing
+#SBATCH --time                  24:00:00
+#SBATCH --gpus                  1
+#SBATCH --output                output.txt
 
 bash scripts.sh
